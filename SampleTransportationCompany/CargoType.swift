2856dd5abf776b6a9b21904376f8c323d032d845
Berk Yeteroglu <berkyeteroglu@icloud.com>
2023-09-24 11:50:58 +0300

    CargoType ve VehicleType enum oluşturuldu.

---
+++ SampleTransportationCompany/CargoType.swift
@@ -0,0 +1,15 @@
+//
+//  CargoType.swift
+//  SampleTransportationCompany
+//
+//  Created by Berk Yeteroğlu on 24.09.2023.
+//
+
+import Foundation
+
+enum CargoType {
+    case DryCargo
+    case LiquidCargo
+    case LiquidFuel
+    case DryFood
+}
---
+++ SampleTransportationCompany/VehicleType.swift
@@ -0,0 +1,15 @@
+//
+//  VehicleType.swift
+//  SampleTransportationCompany
+//
+//  Created by Berk Yeteroğlu on 24.09.2023.
+//
+
+import Foundation
+
+enum VehicleType {
+    case Truck
+    case LongTruck
+    case Train
+    case Tanker
+}
