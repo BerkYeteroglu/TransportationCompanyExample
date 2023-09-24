833e321a9482b0b9cb0c3cc44381f50accc52c90
Berk Yeteroglu <berkyeteroglu@icloud.com>
2023-09-24 11:37:05 +0300

    Porjenin konuları yazıldı. Main. swift oluşturuldu.

--- SampleTransportationCompany/main.swift
+++ SampleTransportationCompany/main.swift
@@ -2,10 +2,21 @@
 //  main.swift
 //  SampleTransportationCompany
 //
+//  Taşımacılık yapan bir firma için yazılım yapılacaktır
+//  Kurumun kamyon, tır, tren, tanker olarak 4 farklı türde aracı vardır (+)
+//  Araçlara yüklenecek yük türleri vardır: Kuru yük, sıvı yük, sıvı yakıt, kuru gıda (+)
+//  Kuruma, iki yer arasında taşınacak yük istekleri gelmekte ve siparişe dönüştürülmekte
+//  Siparişler, ön ödemeli ve sonradan ödemeli olarak  iki halden birisinde oluşturulmaktadır
+//  Her yeni sipariş, şöförü ve taşıma aracı ile birlikte bir projeye dönüştürülmekte ve işletilmektedir
+//  Projeyi oluştururken sadece nesnelerin oluşturulmalarına odaklanın, diğer iş mantıklarını modellemeye gerek yoktur
+//  Nesnelerin ihtiyaç duyduğunuz alanlarını, int gibi basit tiplerden veya String olarak seçin.
+//
 //  Created by Berk Yeteroğlu on 18.09.2023.
 //
 
 import Foundation
 
-print("Hello, World!")
+class Deneme{
+    
+}
 
